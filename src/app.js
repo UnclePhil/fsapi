d3222135c83c3f1a6551928f9e201d7c1fea4d60
Unclephil <koenigphil@gmail.com>
2021-06-05 18:22:07 +0200

    just directories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 "use strict";
 process.EventEmitter = require('events').EventEmitter; 
 const logger = require('console-server');
-const fs = require('fs')
+const fs = require('fs-extra')
 const restify = require('restify');
 const path = require('path')
 const DEBUG = true
@@ -32,15 +32,10 @@ function api_ls(req, res, next) {
   const rdir= req.params.dir || ""
   const ndir = path.join(ACTFS, rdir);
   var files={} ;
-  //logger.warn("req.params", req.params);
-  //logger.warn("rdir", rdir);
-  //logger.warn("ndir", ndir);
   try {
     if (fs.existsSync(ndir)) {
-      fs.readdir(ndir, function (err, files) {
-        //handling error
-          res.send({"action":"ls","dir":rdir,"content":files});
-      });
+      files=fs.readdirSync(ndir, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
+      res.send({"action":"ls","dir":rdir,"exist":true,"content":files});
     }
     else {
       res.status(404)
@@ -54,35 +49,7 @@ function api_ls(req, res, next) {
   next();
 }
 
-
-//=== LD =================================
-// TBC
-function api_ld(req, res, next) {
-  logreq(req);
-  const rdir= req.params.dir || ""
-  const ndir = path.join(ACTFS, rdir);
-  var files={} ;
-  try {
-    if (fs.existsSync(ndir)) {
-      
-      fs.readdir(ndir, function (err, files) {
-        //handling error
-          res.send({"action":"ld","dir":rdir,"content":files});
-      });
-    }
-    else {
-      res.status(404)
-      res.send({"action":"ld","dir":rdir,"exist":false});
-    }
-  } catch(e) {
-    logger.error("LD An error occurred: "+e)
-    res.status(500)
-    res.send('Uncontrolled error occurred - ld');
-  }  
-  next();
-}
-
-//readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
+// readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
 
 //=== EXIST ===============================
 function api_exist(req, res, next) {
@@ -150,8 +117,8 @@ server.use(restify.plugins.bodyParser());
 server.get('/', infos);
 server.get('/ls/', api_ls);
 server.get('/ls/:dir', api_ls);
-server.get('/ld/', api_ld);
-server.get('/ld/:dir', api_ld);
+//server.get('/ld/', api_ld);
+//server.get('/ld/:dir', api_ld);
 server.get('/ex/:dir', api_exist)
 server.post('/md/:dir', api_md);
 
