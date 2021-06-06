2ce7a133d6fca95396e012bb5357a10233a0e973
Unclephil <koenigphil@gmail.com>
2021-06-06 14:58:50 +0200

    work with query parameter

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.FSAPI_PORT || 8000 ;
 const BUILD = process.env.BUILD || "Unknow"
 
 // application infos 
-const appinfos={"app":"fsapi","build":BUILD,"port":PORT,"actfs":ACTFS}
+const appinfos={"app":"fsapi","build":BUILD,"Author":"Unclephil","port":PORT,"actfs":ACTFS}
 
 
 /// == Functions std === 
@@ -27,9 +27,10 @@ function infos(req, res, next) {
   next();
 }
 //=== LS =================================
+// return only Directories
 function api_ls(req, res, next) {
   logreq(req);
-  const rdir= req.params.dir || ""
+  const rdir= req.query.dir || ""
   const ndir = path.join(ACTFS, rdir);
   var files={} ;
   try {
@@ -49,18 +50,18 @@ function api_ls(req, res, next) {
   next();
 }
 
-// readdirSync(source, { withFileTypes: true }).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name)
-
 //=== EXIST ===============================
 function api_exist(req, res, next) {
   logreq(req);
   var resp = {}
+  const rdir= req.query.dir || ""
+  const ndir = path.join(ACTFS, rdir);
   try {
-    if (fs.existsSync(path.join(ACTFS, req.params.dir))) {
-      resp = {"action":"ex","dir":req.params.dir,"exist":true}
+    if (fs.existsSync(ndir)) {
+      resp = {"action":"ex","dir":rdir,"exist":true}
       res.send(resp);
     } else {
-      resp = {"action":"ex","dir":req.params.dir,"exist":false}
+      resp = {"action":"ex","dir":rdir,"exist":false}
       res.status(404)
       res.send(resp);
     }
@@ -77,17 +78,18 @@ function api_exist(req, res, next) {
 //=== MKDIR ===============================
 function api_md(req, res, next) {
   logreq(req);
-  const ndir = path.join(ACTFS, req.params.dir);
-  try {
+  const rdir= req.query.dir || ""
+  const ndir = path.join(ACTFS, rdir);
+    try {
     if (!fs.existsSync(ndir)) {
       fs.mkdirSync(ndir, {
         recursive: true
       });
-      res.send({"action":"md","dir":req.params.dir,"exist":true, "created":true})
+      res.send({"action":"md","dir":rdir,"exist":true, "created":true})
     }
     else {
       res.status(404)
-      res.send({"action":"md","dir":req.params.dir,"exist":true, "created":false})
+      res.send({"action":"md","dir":rdir,"exist":true, "created":false})
     }
   } catch(e) {
     logger.error("MD An error occurred: "+e)
@@ -99,7 +101,6 @@ function api_md(req, res, next) {
 
 // == function error mgt ==
 function logerror(req, res, err, callback) {
-
   logger.error(err.name,err.message)
   callback();
 }
@@ -112,15 +113,13 @@ function logreq(req) {
 var server = restify.createServer();
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
-//server.use(restify.plugins.CORS());
+server.pre(restify.plugins.pre.sanitizePath());
 
 server.get('/', infos);
-server.get('/ls/', api_ls);
-server.get('/ls/:dir', api_ls);
-//server.get('/ld/', api_ld);
-//server.get('/ld/:dir', api_ld);
-server.get('/ex/:dir', api_exist)
-server.post('/md/:dir', api_md);
+server.get('/ls', api_ls);
+//server.get('/ls/:dir', api_ls);
+server.get('/ex', api_exist)
+server.post('/md', api_md);
 
 server.listen(PORT, function() {
   logger.info('%s listening at %s', server.name, server.url);
