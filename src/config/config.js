1b7dcaddaafbbb1fc35b1584a58a26faf65e4bc8
Unclephil <koenigphil@gmail.com>
2021-06-30 17:34:21 +0200

    real rest

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+// default config
+config.port    = 8000;
+config.actfs   = "/mnt"
+
+// security implementation
+config.mdtoken = "TheDefaultTokenMD";
+config.rmtoken = "TheDefaultTokenRM";
+
+module.exports = config;
